2e29a443f0fcbc84ea4cf22adbc2cc6a61bbdb9f
Rui Pereira <ruijosemangas@gmail.com>
2015-04-08 19:47:27 +0100

    readme

--- odin-project/micro-reddit/app/models/post.rb
+++ odin-project/micro-reddit/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 20}
   validates :body, presence: true, length: {maximum: 100}
+  validates :user, presence: true
   belongs_to :user
   has_many :comments
 end
