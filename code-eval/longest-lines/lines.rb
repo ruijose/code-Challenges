f505a1944f2fd6307473ec3c057188e6b2ec2500
Rui Pereira <ruijosemangas@gmail.com>
2015-04-12 17:27:54 +0100

    code eval first exercise

--- code-eval/longest-lines/lines.rb
+++ code-eval/longest-lines/lines.rb
@@ -2,14 +2,14 @@ def lines
   lines = File.open(ARGV.first, 'r').readlines
   numberOutputs = lines[0]
   hash = lines_length(lines[1..-1])
-  ordered_lines(values(hash), numberOutputs.to_i, hash).sort_by(&:length).reverse
+  ordered_lines(hash_values(hash), numberOutputs.to_i, hash).sort_by(&:length).reverse
 end
 
 def lines_length(lines) # return a hash with the length of each line
   lines.each_with_object(Hash.new) { |line,result| result[line] = line.length }
 end
 
-def values(hash) # array with hash values
+def hash_values(hash) # array with hash values
   hash.each_value.each_with_object(Array.new) { |value,result| result << value }
 end
 
