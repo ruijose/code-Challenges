4e58933c052d600d7ceea1e0e6e386ba501cda4c
Rui Pereira <ruijosemangas@gmail.com>
2015-04-12 14:55:34 +0100

    authentication

--- odin-project/sample-rails-app/app/controllers/users_controller.rb
+++ odin-project/sample-rails-app/app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to THE app!"
+      log_in(@user)
       redirect_to @user
     else
       render 'new'
--- odin-project/sample-rails-app/app/helpers/sessions_helper.rb
+++ odin-project/sample-rails-app/app/helpers/sessions_helper.rb
@@ -2,4 +2,18 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    #se o current_user não for nil fica como esta caso contraio fica com a segunda parte
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
