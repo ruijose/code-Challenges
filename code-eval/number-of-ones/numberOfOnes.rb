ce7db91dea3ab09ff6043a43fb5a456b5d20c3ed
Rui Pereira <ruijosemangas@gmail.com>
2015-04-16 12:14:16 +0100

    another one

---
+++ code-eval/number-of-ones/numberOfOnes.rb
@@ -0,0 +1,9 @@
+def ones(file=File.open(ARGV.first,"r"))
+  file.readlines.each { |line| count(line.to_i.to_s(2)) }
+end
+
+def count(binary)
+  puts binary.chars.map(&:to_i).reduce(0) { |result,n| result += 1 if n==1; result}
+end
+
+ones
---
+++ code-eval/number-of-ones/readme.md
@@ -0,0 +1,1 @@
+write a program which determines the number of 1 bits in the internal representation of a given integer.
