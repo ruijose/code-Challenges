11c18a49da471aa3d14ba9b7d5a68eeb8cd7872a
Rui Pereira <ruijosemangas@gmail.com>
2015-04-16 11:40:27 +0100

    codeEval pairs exercise

---
+++ code-eval/numbers-pairs/numberPairs.rb
@@ -0,0 +1,29 @@
+def pairs(file=File.open(ARGV.first,"r"))
+  file.readlines.each do |line|
+    xNumber = line.split(";")[1].to_i
+    numbers = line.split(";")[0].split(",")
+    result = cycles(numbers,xNumber)
+    result << "NULL" if result.empty?
+    puts result.join(";")
+  end
+end
+
+def cycles(numbers,xNumber)
+  result = []
+  numbers.each do |n1|
+    numbers.each do |n2|
+      next unless n1.to_i != n2.to_i
+      if n1.to_i + n2.to_i == xNumber.to_i
+        result << n1.to_s + "," + n2.to_s unless result.include?(n2.to_s + "," + n1.to_s)
+      end
+    end
+  end
+  result
+end
+
+pairs
+
+
+
+
+
---
+++ code-eval/numbers-pairs/readme.md
@@ -0,0 +1,1 @@
+you are given a sorted array of positive integers and a number 'X'. Print out all pairs of numbers whose sum is equal to X. Print out only unique pairs and the pairs should be in ascending order
