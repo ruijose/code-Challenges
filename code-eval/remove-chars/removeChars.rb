fc55537a970024c26e6ed12c65085058b23028e4
Rui Pereira <ruijosemangas@gmail.com>
2015-04-17 10:44:30 +0100

    2nd solution remove-chars problem

--- code-eval/remove-chars/removeChars.rb
+++ code-eval/remove-chars/removeChars.rb
@@ -1,3 +1,4 @@
+#solution 1
 def remove_chars_of(file = File.open(ARGV.first, "r"))
   file.readlines.each do |line|
     split = line.split(",")
@@ -10,4 +11,10 @@ def remove_chars_of(file = File.open(ARGV.first, "r"))
   end
 end
 
-remove_chars_of
+#solution2
+def remove_chars2_of(file = File.open(ARGV.first, "r"))
+  file.readlines.each do |line|
+    mainString, removeString = line.split(",").map(&:strip) #mass assign like a boss
+    puts removeString.chars.reduce(mainString) { |result,char| result.gsub(char, "") }
+  end
+end
