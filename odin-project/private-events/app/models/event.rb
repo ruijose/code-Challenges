806ca8b6031780f705e68f825902d7218e5bef22
Rui Pereira <ruijosemangas@gmail.com>
2015-06-01 18:43:22 +0100

    final commit

--- odin-project/private-events/app/models/event.rb
+++ odin-project/private-events/app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-  belongs_to :creator, :class_name => "User"
-  has_many :invites,   :foreign_key => "attended_event_id"
-  has_many :attendees, :through => :invites
+  belongs_to :creator,   :class_name => "User"
+  has_many   :invites,   :foreign_key => "attended_event_id"
+  has_many   :attendees, :through => :invites
 
   validates :name,        presence: true
   validates :location,    presence: true
