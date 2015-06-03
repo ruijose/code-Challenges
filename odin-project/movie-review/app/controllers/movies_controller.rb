2e6e4a0741c9185fa0f070ebc0fb4f9e0b08fe2b
Rui Pereira <ruijosemangas@gmail.com>
2015-06-03 22:28:59 +0100

    add pictures to movies; paperclip gem

--- odin-project/movie-review/app/controllers/movies_controller.rb
+++ odin-project/movie-review/app/controllers/movies_controller.rb
@@ -27,6 +27,6 @@ class MoviesController < ApplicationController
 
   private
   def movie_params
-    params.require(:movie).permit(:title, :description, :length, :director,:rating)
+    params.require(:movie).permit(:title, :description, :length, :director, :rating, :image)
   end
 end
