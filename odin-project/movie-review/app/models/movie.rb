1fec739b475858e06919fd9b0bdfff6ca731e42e
Rui Pereira <ruijosemangas@gmail.com>
2015-06-06 11:57:23 +0100

    figure validation

--- odin-project/movie-review/app/models/movie.rb
+++ odin-project/movie-review/app/models/movie.rb
@@ -5,9 +5,10 @@ class Movie < ActiveRecord::Base
   validates :length,      presence: true, numericality: true
   validates :director,    presence: true
   validates :rating,      presence: true
+  validates :image,       :attachment_presence => true
 
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "400x600#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
