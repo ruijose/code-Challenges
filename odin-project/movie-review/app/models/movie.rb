7e691de695c0f99e6c62431e51959e3436bbc7b9
Rui Pereira <ruijosemangas@gmail.com>
2015-06-16 14:37:22 +0100

    layout changed

--- odin-project/movie-review/app/models/movie.rb
+++ odin-project/movie-review/app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   validates :image,       :attachment_presence => true
 
   belongs_to :user
-  has_many :reviews
+  has_many   :reviews
 
   has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
--- odin-project/movie-review/app/models/review.rb
+++ odin-project/movie-review/app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
+  validates  :rating, :inclusion => 1..5
+  validates  :comment, presence: true
 end
+
